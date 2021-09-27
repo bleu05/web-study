4aa16c45058b407cefe71bfe383d3383e321efec
bleu05 <81405678+bleu05@users.noreply.github.com>
2021-09-27 18:32:38 +0900

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,20 @@
+<meta charset="UTF-8">
+<!DOCTYPE html>
+
+
+<html>
+  <head>
+    <title>
+      사이트에서 로그아웃 합니다.
+    <title>
+    </head>
+
+    <body>
+      <br>
+      <?php
+      session_start();
+      session_destroy();
+      header('location : ./login.php');
+      ?>
+    </body>
+</html>
