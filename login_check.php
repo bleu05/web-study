6d1ced600499eb9004b6f129bcb7a7448bca12e5
bleu05 <81405678+bleu05@users.noreply.github.com>
2021-09-27 18:32:22 +0900

    Add files via upload

---
+++ login_check.php
@@ -0,0 +1,35 @@
+<meta charset="UTF-8">
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>로그인 작동 페이지/title>
+  </head>
+  <body>
+    <?php
+    session_start();
+
+    $servername="localhost";
+    $username="root"
+    $password="alpha0995";
+    $database="web_db";
+    $con=mysqli_connect($servername, $username, $password, $database)
+    or die("Mysql Connect Error!");
+
+    $sel_query="SELECT * FROM member WHERE id LIKE '{$_POST['id']}';";
+    $sel_result=mysqli_query($con,$sel_query) or die("Query Error!");
+    $count=mysqli_num_rows($sel_result);
+
+    if($count==0){
+
+    }
+    else {
+      if(){
+
+      }
+      else{
+
+      }
+    }
+    ?>
+  </body>
+</html>
